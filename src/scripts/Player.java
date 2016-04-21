f37accb8249d3b9d0322a2a579edd31d0d2502c1
jxue8 <jxue8@students.d125.org>
2016-04-20 22:16:31 -0500

    changed player.java

--- src/scripts/Player.java
+++ src/scripts/Player.java
@@ -1,6 +1,4 @@
-<<<<<<< HEAD
 package scripts;
-import java.util.*;
 
 public class Player extends Deck {
 
@@ -61,34 +59,3 @@ public class Player extends Deck {
 }
 
 
-
-
-
-=======
-package scripts;
-
-public class Player extends Deck {
-
-	Card card1;
-	Card card2;
-	int points;
-
-	public Player() {
-		card1 = deal();
-		card2 = deal();
-		points = 10000;
-	}
-
-	public int handMeter(){	
-		int meter = 1; // Meter Goes From 1 - 10
-		if (card1.getNumber() == card2.getNumber()) {
-			if (card1.getNumber() <= 5) meter = 5;
-			else if (card1.getNumber() <= 10) meter = 7;
-			else meter = 9;
-		}
-		return meter;
-	}
-}
-
-
->>>>>>> origin/master
