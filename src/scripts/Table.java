afdb0bcadefff62951f336e1cb64267299e2e2b1
jxue8 <jxue8@students.d125.org>
2016-04-20 21:01:37 -0500

    Beginning Of Table Class

--- src/scripts/Table.java
+++ src/scripts/Table.java
@@ -1,5 +1,9 @@
 package scripts;
 
-public class Table {
+public class Table extends Deck{
 	//TODO create a table
+	Deck aDeck = new Deck();
+	Computer c1 = new Computer();
+    User u1 = new User();
+	
 }
