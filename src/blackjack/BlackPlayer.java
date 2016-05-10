5830ea23b44dafa7b13ebf0e4a9e103f7e60bc33
wilson <wzhu8@students.d125.org>
2016-05-10 15:19:24 -0500

    finished black hand

--- src/blackjack/BlackPlayer.java
+++ src/blackjack/BlackPlayer.java
@@ -1,7 +1,16 @@
 package blackjack;
 import java.util.*;
+import scripts.Card; 
 
-import holdem.Hand;
 public class BlackPlayer {
-	private ArrayList<Card> hand = new Hand();
+	private BlackHand hand = new BlackHand();
+
+	
+
+
+
+
+
+
+
 }
