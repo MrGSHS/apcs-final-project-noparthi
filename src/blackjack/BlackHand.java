2f87d78d1c43172127fe5ae0fc29e014bd1adeda
wilson <wzhu8@students.d125.org>
2016-05-12 15:01:22 -0500

    black hand mesdd up

--- src/blackjack/BlackHand.java
+++ src/blackjack/BlackHand.java
@@ -1,5 +1,22 @@
 package blackjack;
-
+import scripts.Card;
+import scripts.Deck;
+import java.util.*;
 public class BlackHand {
-
+	private static Deck deck1=new Deck();
+	private ArrayList<Card> hand1;
+	
+	public BlackHand(){
+		hand1= new ArrayList<Card>();
+		hand1.add(deck1.deal());
+		hand1.add(deck1.deal());
+	}
+	
+	public ArrayList<Card> getCards(){
+		return hand1;
+		
+	}
+	public void restart(){
+		deck1=new Deck();
+	}
 }
