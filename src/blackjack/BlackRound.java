39c996baa5fcabfbb8c56ee70666aa2a00d763bb
Dpock <dmopart8@students.d125.org>
2016-05-16 15:25:59 -0500

    worked on blackround

--- src/blackjack/BlackRound.java
+++ src/blackjack/BlackRound.java
@@ -4,24 +4,27 @@ import holdem.Player;
 
 public class BlackRound {
 	private static int roundNumber = 0;
-	Player 
-
-	public Round(Game game) {
+	private static BlackPlayer play=new BlackPlayer();
+	private static BlackComp deal=new BlackComp();
+	private int bet;
+	public BlackRound(int playerBet){
+		play.makeBet(playerBet);
+		bet=playerBet;
 		
+	}
+	public void updateHandAfterPlayerHits (){
+		play.hit();
+		if (play.calc()>21){
+			deal.demGains(2*bet);
 		}
-	
-
-	public int getRoundNumber() {
-		return roundNumber;
+			
+			
 	}
-
 	
-
-	//Check To See If Should Move To Next Round
-	public boolean moveOn() {
 		
 	}
-	
-}
+		
+	}
+
 
 
