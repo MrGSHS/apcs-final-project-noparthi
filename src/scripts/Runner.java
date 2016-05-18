c39eb8cf573c8a65024b719a201d4468927dc3cf
jxue8 <jxue8@students.d125.org>
2016-05-18 16:22:52 -0500

    Minor Fix

--- src/scripts/Runner.java
+++ src/scripts/Runner.java
@@ -4,7 +4,7 @@ public class Runner {
 	public static void main(String[] args) {
 		LogoDrop dropLogo = new LogoDrop();
 		while(!dropLogo.getLogoDone()) {
-			System.out.print("");
+			System.out.print("Loading...");
 		}
 		dropLogo.dispose();
 		new ChooserDisplay();
